de1419a772567a7f33fc4777107e52325a2d3ef4
Vinh2608 <vinhluu1996@gmail.com>
2024-01-20 12:02:49 +0700

    update

--- dataset1.sh
+++ dataset1.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-export semi_setting='dataset1/1_4/split_random/deeplabv3plus/self_training++/resnet101/consistency_training/second_time'
+export semi_setting='dataset1/1_4/split_random/deeplabv3plus/self_training/resnet50/non_consistency_training/second_time'
 python3 -W ignore main.py \
 --dataset dataset1 --config configs/dataset1.yaml --data-root ./  \
---batch-size 16 --backbone resnet101 --model deeplabv3plus \
+--batch-size 16 --backbone resnet50 --model deeplabv3plus \
 --labeled-id-path dataset/splits/dataset1/1_4/split_random/labeled.txt \
 --unlabeled-id-path dataset/splits/dataset1/1_4/split_random/unlabeled.txt \
 --pseudo-mask-path outdir/pseudo_masks/$semi_setting \
---save-path outdir/models/$semi_setting --plus --reliable-id-path outdir/reliable_ids/$semi_setting
+--save-path outdir/models/$semi_setting \
+--time 2 \
+--consistency_training False #--plus --reliable-id-path outdir/reliable_ids/$semi_setting
--- dataset2.sh
+++ dataset2.sh
@@ -1,8 +1,8 @@
-export semi_setting='dataset2/1_4/split_random/deeplabv3plus/self_training/resnet101/consistency_training/second_time'
+export semi_setting='dataset2/1_4/split_random/deeplabv3plus/self_training/resnet50/non_consistency_training/first_time'
 python3 -W ignore main.py \
 --dataset dataset2 --config configs/dataset2.yaml --data-root ./  \
---batch-size 16 --batch-size-consistency 4 --backbone resnet101 --model deeplabv3plus \
+--batch-size 16 --batch-size-consistency 4 --backbone resnet50 --model deeplabv3plus \
 --labeled-id-path dataset/splits/dataset2/1_4/split_random/labeled.txt \
 --unlabeled-id-path dataset/splits/dataset2/1_4/split_random/unlabeled.txt \
 --pseudo-mask-path outdir/pseudo_masks/$semi_setting \
---save-path outdir/models/$semi_setting #--plus --reliable-id-path outdir/reliable_ids/$semi_setting
+--save-path outdir/models/$semi_setting --time 1 --consistency_training False #--plus --reliable-id-path outdir/reliable_ids/$semi_setting
